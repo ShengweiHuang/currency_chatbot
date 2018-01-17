6baa2d6c3810af31e4c00d3769f5b351be849d0b
ShengweiHuang <huang6275@gmail.com>
2018-01-18 01:38:47 +0700

    update

--- currency.py
+++ currency.py
@@ -6,14 +6,18 @@ CURRENCY_URL = "http://rate.bot.com.tw/xrt/flcsv/0/day"
 CURRENCY_FILE_NAME = "currency_data.csv"
 
 def get_currency_data():
-	return_list = []
+	currency_list = []
 	urllib.request.urlretrieve(CURRENCY_URL, CURRENCY_FILE_NAME)
 	with open(CURRENCY_FILE_NAME, "r") as csvfile:
 		spamreader = csv.reader(csvfile, delimiter=',', quotechar=',')
 		for row in spamreader:
 			return_list.append(row)
 	os.remove(CURRENCY_FILE_NAME)
-	return return_list
+	return currency_list
+
+def get_currency_id(currency_type):
+	currency_data = get_currency_data()
+	
 
 if __name__ == "__main__":
 	get_currency_data()
