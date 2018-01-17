3adf7ee5e5dc79b9d5f6c638989846929d18bfc2
ShengweiHuang <huang6275@gmail.com>
2018-01-17 15:08:48 +0700

    update

--- app.py
+++ app.py
@@ -27,9 +27,7 @@ def callback():
 def handle_message(event):
     print("Handle: reply_token: " + event.reply_token + ", message: " + event.message.text)
     content = "{}: {}".format(event.source.user_id, event.message.text)
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=content))
+    line_bot_api.reply_message(TextSendMessage(text=content))
 
 if __name__ == "__main__":
     app.run(host=os.getenv('IP','0.0.0.0'), port=int(os.getenv('PORT','8080')))
