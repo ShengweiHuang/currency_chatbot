16aec537148eeca7d3b5f7baadcec471b902575c
ShengweiHuang <huang6275@gmail.com>
2018-01-18 14:05:50 +0700

    update

--- text_convert.py
+++ text_convert.py
@@ -1,7 +1,10 @@
-import os
+import jieba
 
 def get_output_str(input_str = ""):
-	return "get_output_str"
+	return str(string_to_list(input_str))
+
+def string_to_list(input_str = ""):
+	return (list)(jieba.cut(input_str))
 
 if __name__ == "__main__":
 	get_output_str()
